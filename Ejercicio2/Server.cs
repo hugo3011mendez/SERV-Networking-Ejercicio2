591303ba76072ce9762a40be78c4fbdcb83e7f0c
Hugo Mendez <hugo3011mendez@gmail.com>
2021-01-25 18:59:53 +0100

    - Añadido control de puerto ocupado al servidor.

--- Ejercicio2/Server.cs
+++ Ejercicio2/Server.cs
@@ -76,7 +76,29 @@ namespace Ejercicio2
         {
             IPEndPoint ie = new IPEndPoint(IPAddress.Loopback, 31416); // Creo y defino el IPEndPoint del server
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Creo y defino el socket
-            s.Bind(ie); // Enlazo el socket al IPEndPoint
+
+            try
+            {
+                s.Bind(ie); // Enlazo el socket al IPEndPoint
+            }
+            catch (SocketException e) when (e.ErrorCode == (int)SocketError.AddressAlreadyInUse)
+            {
+                // Si está ocupado, lo cambio a otro secundario
+                ie.Port = 31415;
+                try
+                {
+                    s.Bind(ie);
+                    Console.WriteLine("Servidor lanzado en el puerto " + ie.Port);
+                }
+                catch (SocketException e1) when (e1.ErrorCode == (int)SocketError.AddressAlreadyInUse)
+                {
+                    // Si el secundario también está ocupado, cierro el server
+                    Console.WriteLine("Puertos ocupados, no se puede lanzar el servidor");
+                    s.Close();
+                    return;
+                }
+            }
+
             s.Listen(30); // Se queda esperando una conexión y se establece la cola a 30
 
             while (true)
